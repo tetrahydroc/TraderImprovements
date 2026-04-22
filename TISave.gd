abeea056c6dd98781c04d2fc3d127e5e6e0c7d64
tetrahydroc <tetrahydroc@gmail.com>
2026-04-22 17:41:40 -0400

    v2-hooks

---
+++ TISave.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+@export var rep: Dictionary = {}
+@export var daily_seed: int = 0
+@export var daily_day: int = 0
+@export var daily_completed: Dictionary = {}
+@export var noted_dailies: Array = []
+@export var kill_progress: Dictionary = {}
+@export var contracts: Dictionary = {}
